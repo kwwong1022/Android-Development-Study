cb1ed93dc8bf8a33b3278e59d860400e2f14f0de
Kw Wong <67175089+kwwong1022@users.noreply.github.com>
2021-08-30 00:06:24 +0800

    v 0.0.3

--- 04 Button Counter/app/src/main/java/udemy/course/buttoncounter/MainActivity.java
+++ 04 Button Counter/app/src/main/java/udemy/course/buttoncounter/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private Button button;
     private TextView textView;
     private static final String TAG = "MainActivity";
+    private final String TEXT_CONTENT = "TextContent";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,41 +45,59 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onStart() {
+        Log.d(TAG, "onStart: in");
         super.onStart();
+        Log.d(TAG, "onStart: out");
     }
 
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        Log.d(TAG, "onRestoreInstanceState: in");
         super.onRestoreInstanceState(savedInstanceState);
+        textView.setText(savedInstanceState.getString(TEXT_CONTENT));
+        Log.d(TAG, "onRestoreInstanceState: out");
     }
 
     @Override
     protected void onResume() {
+        Log.d(TAG, "onResume: in");
         super.onResume();
+        Log.d(TAG, "onResume: out");
     }
 
     @Override
     protected void onPause() {
+        Log.d(TAG, "onPause: in");
         super.onPause();
+        Log.d(TAG, "onPause: out");
     }
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
+        Log.d(TAG, "onSaveInstanceState: in");
+        outState.putString(TEXT_CONTENT, textView.getText().toString());
         super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState: out");
     }
 
     @Override
     protected void onStop() {
+        Log.d(TAG, "onStop: in");
         super.onStop();
+        Log.d(TAG, "onStop: out");
     }
 
     @Override
     protected void onDestroy() {
+        Log.d(TAG, "onDestroy: in");
         super.onDestroy();
+        Log.d(TAG, "onDestroy: out");
     }
 
     @Override
     protected void onRestart() {
+        Log.d(TAG, "onRestart: in");
         super.onRestart();
+        Log.d(TAG, "onRestart: out");
     }
 }
