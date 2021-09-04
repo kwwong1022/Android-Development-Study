7a2dc82dc4bbb325a88146c80617d0203793a5c3
Kw Wong <67175089+kwwong1022@users.noreply.github.com>
2021-09-05 06:15:29 +0800

    v 0.1.1

--- 07 Top Ten Downloaded App/app/src/main/java/udemy/course/toptendownloader/MainActivity.java
+++ 07 Top Ten Downloaded App/app/src/main/java/udemy/course/toptendownloader/MainActivity.java
@@ -2,15 +2,43 @@ package udemy.course.toptendownloader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate: starting AsyncTask");
+        DownloadData downloadData = new DownloadData();
+        downloadData.execute("URL goes here");
+        Log.d(TAG, "onCreate: done");
     }
 
+    private class DownloadData extends AsyncTask<String, Void, String> {
+        private static final String TAG = "DownloadData";
+
+        @Override
+        protected String doInBackground(String... strings) {
+            Log.d(TAG, "doInBackground: starts with " + strings[0]);
+            //String rssFeed = downloadXML(strings[0]);
+            //if (rssFeed == null) {
+            //    Log.e(TAG, "doInBackground: Error downloading");
+            //}
+            //return rssFeed;
+            return "";
+        };
+
+        @Override
+        protected void onPostExecute(String s) {
+            Log.d(TAG, "onPostExecute: parameter is: " + s);
+            super.onPostExecute(s);
+        }
+    }
 }
--- SM3607 Course Material/Lec3ActivityDemo/app/src/main/java/scm/kaifwong8/activitydemo/MainActivity.java
+++ SM3607 Course Material/Lec3ActivityDemo/app/src/main/java/scm/kaifwong8/activitydemo/MainActivity.java
@@ -2,7 +2,9 @@ package scm.kaifwong8.activitydemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button start = findViewById(R.id.btn_start_activity);
+        start.setOnClickListener((v) -> {
+            Intent i = new Intent(MainActivity.this, SimpleActivity.class);
+            startActivity(i);
+        });
     }
 }
